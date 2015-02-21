91c1ab13b00fd8e3f62429fd704763ec35dff699
Null <foo.null@yahoo.com>
2015-02-20 20:31:10 -0500

    Updating patch to include lobby, multi-monitor changes

--- src/mavericks_patch.c
+++ src/mavericks_patch.c
@@ -25,15 +25,76 @@
  */
 
  // To compile:
- // clang mavericks_patch.c -framework ApplicationServices -dynamiclib -m32 -init _mavericks_patch_init -flat_namespace -mmacosx-version-min=10.5 -o mavericks_patch.dylib
+ // clang mavericks_patch.c -framework ApplicationServices -dynamiclib -m32 -mmacosx-version-min=10.5 -o mavericks_patch.dylib
 
  #include <sys/mman.h>
+ #include <unistd.h>
  #include <ApplicationServices/ApplicationServices.h>
+ #include <mach/mach_init.h>
+ #include <mach/mach_vm.h>
+ #include <mach/task.h>
+ #include <mach/mach_port.h>
 
-void mavericks_patch_init()
+ #define NOP_BYTE 0x90
+
+ static void writeProtectedBuffer(void *buffer, size_t size, uintptr_t address)
+ {
+ 	mach_vm_protect(mach_task_self(), address, size, 0, VM_PROT_ALL);
+ 	memcpy((void *)address, buffer, size);
+ 	mach_vm_protect(mach_task_self(), address, size, 0, PROT_READ | PROT_EXEC);
+ }
+
+static void nopInstructions(uintptr_t address, size_t size)
+{
+	void *nopBuffer = malloc(size);
+	memset(nopBuffer, NOP_BYTE, size);
+	writeProtectedBuffer(nopBuffer, size, address);
+	free(nopBuffer);
+}
+
+static void changeGamespyAddress(uintptr_t pointer, const char *newAddress)
+{
+	strcpy((char *)pointer, newAddress);
+}
+
+static void patchGameVersionInstructions(char *gameVersionPatchBuffer, uintptr_t address, uintptr_t offset1, uintptr_t offset2)
+{
+	writeProtectedBuffer(gameVersionPatchBuffer, 2, address + offset1);
+	writeProtectedBuffer(gameVersionPatchBuffer, 1, address + offset2);
+}
+
+static __attribute__((constructor)) void init()
 {
 	// Reserve memory halo wants before halo initiates, should help fix a bug in 10.9 where GPU drivers may have been loaded here
 	mmap((void *)0x40000000, 0x1b40000, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_ANON | MAP_PRIVATE, -1, 0);
+
+	// nop two jmps which Halo checks if system has AGL_ACCELERATED
+	// this prevents Halo from running in certain display setups and it can do just fine if we skip these checks..
+	nopInstructions(0x2B46CB, 2);
+	nopInstructions(0x2B482E, 2);
+
+	// Use the new HPC game browser since Gamespy died
+	changeGamespyAddress(0x3867A6, "natneg1.hosthpc.com");
+	changeGamespyAddress(0x386B2C, "s1.master.hosthpc.com");
+	changeGamespyAddress(0x38BF5B, "s1.ms01.hosthpc.com");
+	changeGamespyAddress(0x3867C0, "natneg2.hosthpc.com");
+
+	// Change 01.00.09.0620 to 01.00.10.0621; affects hosting server
+	// This is not stored in memory as a simple string, very convoluted/strange
+	char gameVersionPatchBuffer[] = {0x31, 0x30};
+	writeProtectedBuffer(gameVersionPatchBuffer, 2, 0x114da9);
+	writeProtectedBuffer(gameVersionPatchBuffer, 1, 0x114dbc);
+
+	// Change 4 locations that check against game version
+	patchGameVersionInstructions(gameVersionPatchBuffer, 0x114C7C, 11, 24);
+	patchGameVersionInstructions(gameVersionPatchBuffer, 0x114d97, 18, 37);
+	patchGameVersionInstructions(gameVersionPatchBuffer, 0x114fef, 18, 37);
+	patchGameVersionInstructions(gameVersionPatchBuffer, 0x237e17, 15, 28);
+
+	// Change 1.08 string (01.00.08.0616) to 1.09 (01.00.09.0620)
+	// The lobby will show both 1.10 and 1.09 servers
+	char oldVersionString[] = "01.00.09.0620";
+	writeProtectedBuffer(oldVersionString, sizeof(oldVersionString) - 1, 0x323B14);
 
 	// Make sure we're frontmost app
 	ProcessSerialNumber serialNumber;
